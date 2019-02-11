91db01fd0838e11074374d7bbb0985cd56ca41bc
Alex Symbol <simvol710@gmail.com>
2019-02-11 02:17:22 +0200

    added order check, endind the day

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -13,19 +13,26 @@
 						type="number"
 						class="form-control"
 						placeholder="Quantity"
-						v-model="quantity">
+						v-model="quantity"
+						:class="{danger: insufficientQuantity}">
 				</div>
 				<div class="pull-right">
 					<button
 						class="btn btn-success"
 						@click="sellStock"
-						:disabled="quantity <= 0"
-						>Sell</button>
+						:disabled="insufficientQuantity || quantity <= 0"
+						>{{ insufficientQuantity ? 'Not enough' : 'Sell' }}</button>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
+
+<style scoped>
+	.danger {
+		border: 1px solid red
+	}
+</style>
 
 <script>
 	import { mapActions } from 'vuex'
@@ -34,6 +41,11 @@
 		data() {
 			return {
 				quantity: 0
+			}
+		},
+		computed: {
+			insufficientQuantity() {
+				return this.quantity > this.stock.quantity
 			}
 		},
 		methods: {
